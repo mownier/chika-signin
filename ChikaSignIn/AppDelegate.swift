618ccef32874b7dda13dc8294410e09d26fb8fb6
Mounir Ybanez <rinuom91@gmail.com>
2018-02-05 13:58:00 +0800

    Used Sign In module.

--- ChikaSignIn/AppDelegate.swift
+++ ChikaSignIn/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import ChikaCore
+import FirebaseCommunity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,13 +16,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let output: (Result<OK>) -> Void = { result in
-            print(result)
-        }
         let scene = Factory().withOutput(output).build()
         let nav = UINavigationController(rootViewController: scene)
+        scene.title = "Sign In"
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
+        
+        FirebaseApp.configure()
         return true
+    }
+    
+    func output(_ result: Result<OK>) {
+        switch result {
+        case .ok(let ok):
+            showAlert(withTitle: "Success", message: "\(ok)", from: window!.rootViewController!)
+        
+        case .err(let error):
+            showAlert(withTitle: "Error", message: "\(error)", from: window!.rootViewController!)
+        }
+    }
+    
+}
+
+func showAlert(withTitle title: String, message: String, from parent: UIViewController) {
+    DispatchQueue.main.async {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        parent.present(alert, animated: true, completion: nil)
     }
 }

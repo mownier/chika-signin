6d78c9005bb28d7c91812a60f198001e08777b2d
Mounir Ybanez <rinuom91@gmail.com>
2018-02-12 21:48:06 +0800

    Added switchers and helper functions.

--- ChikaSignIn/AppDelegate.swift
+++ ChikaSignIn/AppDelegate.swift
@@ -8,34 +8,83 @@
 
 import UIKit
 import ChikaCore
+import ChikaFirebase
 import FirebaseCommunity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var onlineOperator: OnlinePresenceSwitcherOperator!
+    var offlineOperator: OfflinePresenceSwitcherOperator!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let scene = Factory().withOutput(output).build()
-        let nav = UINavigationController(rootViewController: scene)
-        scene.title = "Sign In"
-        window?.rootViewController = nav
+        FirebaseApp.configure()
+        
+        onlineOperator = OnlinePresenceSwitcherOperation()
+        offlineOperator = OfflinePresenceSwitcherOperation()
+        
+        hasAuth ? showSignOut() : showSignIn()
+        
         window?.makeKeyAndVisible()
         
-        FirebaseApp.configure()
         return true
     }
     
-    func output(_ result: Result<OK>) {
-        switch result {
-        case .ok(let ok):
-            showAlert(withTitle: "Success", message: "\(ok)", from: window!.rootViewController!)
-        
-        case .err(let error):
-            showAlert(withTitle: "Error", message: "\(error)", from: window!.rootViewController!)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        let switcher = OnlinePresenceSwitcher()
+        let completion: (Result<OK>) -> Void = { result in
+            print("[AppDelegate] switch to online:", result)
         }
+        let _ = onlineOperator.withCompletion(completion).switchToOnline(using: switcher)
     }
     
+    func applicationWillResignActive(_ application: UIApplication) {
+        let switcher = OfflinePresenceSwitcher()
+        let completion: (Result<OK>) -> Void = { result in
+            print("[AppDelegate] switch to offline:", result)
+        }
+        let _ = offlineOperator.withCompletion(completion).switchToOffline(using: switcher)
+    }
+    
+}
+
+var hasAuth: Bool {
+    guard let uid = FirebaseCommunity.Auth.auth().currentUser?.uid, !uid.isEmpty else {
+        return false
+    }
+    
+    return true
+}
+
+func showSignOut() {
+    let bundle = Bundle(for: ViewController.self)
+    let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+    let vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    delegate.window?.rootViewController = vc
+}
+
+func showSignIn() {
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let scene = Factory().withOutput(output).build()
+    let nav = UINavigationController(rootViewController: scene)
+    scene.title = "Sign In"
+    delegate.window?.rootViewController = nav
+}
+
+func output(_ result: Result<OK>) {
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let window = delegate.window
+    
+    switch result {
+    case .ok(let ok):
+        showSignOut()
+        showAlert(withTitle: "Success", message: "\(ok)", from: window!.rootViewController!)
+    
+    case .err(let error):
+        showAlert(withTitle: "Error", message: "\(error)", from: window!.rootViewController!)
+    }
 }
 
 func showAlert(withTitle title: String, message: String, from parent: UIViewController) {
